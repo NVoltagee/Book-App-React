c4103997e37a9d1411fc8236d561883dcf7a2317
Norman Trujillo <natrujillo9@gmail.com>
2018-06-13 12:19:46 -0400

    Created books reducer.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript For Dummies " },
+    { title: "HTML & CSS by Jon Duckett" },
+    { title: "Javascript and jQuery by Jon Duckett" },
+    { title: "Eloquent JavaScript" }
+  ];
+}
