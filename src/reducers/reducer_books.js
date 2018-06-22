49ecba4774a075812bad26c1d1ca4aa1947c5a95
Norman Trujillo <natrujillo9@gmail.com>
2018-06-22 16:07:14 -0400

    Added Book pages.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: "Javascript For Dummies " },
-    { title: "HTML & CSS by Jon Duckett" },
-    { title: "Javascript and jQuery by Jon Duckett" },
-    { title: "Eloquent JavaScript" }
+    { title: "Javascript For Dummies", pages: 101 },
+    { title: "HTML & CSS by Jon Duckett", pages: 30 },
+    { title: "Javascript and jQuery by Jon Duckett", pages: 400 },
+    { title: "Eloquent JavaScript", pages: 600 }
   ];
 }
